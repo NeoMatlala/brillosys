c6ceec02fc55d08ca3fbbe6a9683996930ceef3b
Neo Matlala <matlalan@dv8techgroup.com>
2021-09-14 07:58:53 +0200

    portfolio & contact page |started  mobile dropdown

--- main.js
+++ main.js
@@ -28,27 +28,27 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 
-/* --------------- SCROLL SECTION ACTIVE LINK ------------ */
-const sections = document.querySelectorAll('section[id]')
+// /* --------------- SCROLL SECTION ACTIVE LINK ------------ */
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive() {
-    const scrollY = window.pageYOffset
+// function scrollActive() {
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
+//     sections.forEach(current => {
+//         const sectionHeight = current.offsetHeight
+//         const sectionTop = current.offsetTop - 50
 
-        sectionId = current.getAttribute('id')
+//         sectionId = current.getAttribute('id')
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
+//         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         } else {
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
 
-window.addEventListener('scroll', scrollActive)
+// window.addEventListener('scroll', scrollActive)
 
 
 
